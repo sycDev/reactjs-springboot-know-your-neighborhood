701d01fb31d40187112c91ef57668de492513c14
sycDev <sinyi.20028@gmail.com>
2023-07-26 13:02:45 +0800

    initial commit

---
+++ springboot-backend/src/main/java/com/wou/kyn/KynApplication.java
@@ -0,0 +1,13 @@
+package com.wou.kyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KynApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/src/test/java/com/wou/kyn/KynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wou.kyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KynApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
