45d085faeb80d4c992411ccfccdeee6594a78894
sycDev <sinyi.20028@gmail.com>
2023-07-28 10:56:44 +0800

    feat: backend slack webhook integration

---
+++ springboot-backend/src/main/java/com/wou/kyn/webhook/pojo/SlackMessage.java
@@ -0,0 +1,13 @@
+package com.wou.kyn.webhook.pojo;
+
+public class SlackMessage {
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
